a1ce319fe9d4cb3174f1a23d169cfbfb56a4be23
Rajesh-Reel <rajreel2@hotmail.com>
2022-12-03 22:19:56 +0000

    added more pages and styled the navbar, works successfully

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,28 @@
-
 import React from "react";
-import { Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home.js";
+import Books from "./pages/books.js";
+import Food from "./pages/food.js";
+import Sports from "./pages/sports.js";
+import "./nav.css"
 
 function App() {
   return (
     // <Router>
     <>
-      <nav>
-        <ul>
+      <nav className="navbar">
+        <ul className="navbar-contents">
           <li><Link to="/">Home</Link></li>
-          {/* <li><Link to="/books">Books</Link></li> */}
+          <li><Link to="/books">Books</Link></li>
+          <li><Link to="/food">Food</Link></li>
+          <li><Link to="/sports">Sports</Link></li>
         </ul>
       </nav>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/sports" element={<Sports />} />
       </Routes>
     </>
     // </Router>
